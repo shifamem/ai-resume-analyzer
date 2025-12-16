9d4173c4289e8872e86cd4a34533baab03181f6d
shifamem <mshifa559@gmail.com>
2025-12-16 11:14:44 +0000

    Module 1: Implement user authentication with JWT

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./.env" }); // 👈 FIX
+connectDB();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth.routes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on port ${PORT}`)
+
+);
+console.log("ENV CHECK:", process.env.MONGO_URI);
+
